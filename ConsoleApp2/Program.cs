4b24ec9f787a3ed328ef29a7be9139cc1a3663fc
Admin <Admin@TEB123004>
2023-09-29 14:52:05 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uppgift_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = double.Parse(Console.ReadLine());
+            double z = double.Parse(Console.ReadLine());
+            double a = double.Parse(Console.ReadLine());
+            double b = x + z + a;
+            double l = b / 3;
+            Console.WriteLine("medellönet blir " + l + "kr");
+            Console.ReadKey();
+
+
+        }
+    }
+}
